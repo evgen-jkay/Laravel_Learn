014a6eda7727d8c7faec4d249db5913dd026b931
Evgen <evgenlandarenko@gmail.com>
2021-10-06 20:26:08 +0300

    Update 2014_10_12_100000_create_password_resets_table.php

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -13,6 +13,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
