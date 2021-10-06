3bf6809cec7ce1054512451d432f2263a195a3ef
Evgen <evgenlandarenko@gmail.com>
2021-10-06 20:25:38 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
